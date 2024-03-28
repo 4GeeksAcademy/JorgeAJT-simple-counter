f41454e1d6220eba944200b365c96d8a327e5382
Jorge Jiménez <154094092+JorgeAJT@users.noreply.github.com>
2024-03-28 16:27:57 +0000

    errores arreglados

--- src/js/component/secondscounter.jsx
+++ src/js/component/secondscounter.jsx
@@ -7,7 +7,6 @@ const SecondsCounter = (props) => {
     <div className="d-flex justify-content-center mt-5 p-4"> 
         <Digit number = {<i className="fa-solid fa-stopwatch"></i>}/>
         <Digit number = {digit[0]}/>
-        <Digit number = {digit[0]}/>
         <Digit number = {digit[1]}/>
         <Digit number = {digit[2]}/>
         <Digit number = {digit[3]}/>
